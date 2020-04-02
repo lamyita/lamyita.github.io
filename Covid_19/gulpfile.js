55b5fb6a9707f6fc07289e90b167906c3097e74b
lamyita <50775487+lamyita@users.noreply.github.com>
2020-04-02 21:58:47 +0100

    first commit

---
+++ Covid_19/gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+// gulp.task('sass', function () {
+//   return gulp.src('src/scss/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest( 'src/css'));
+// });
+function compile_sass() {
+    return gulp
+      .src("src/sass/**/*.scss")
+      .pipe(sass().on("error", sass.logError))
+      .pipe(gulp.dest('src/css'));
+  }
+  function watchSass() {
+    return gulp.watch("src/scss/**/*.scss", compile_sass);
+  }
+  gulp.task("watch:sass", watchSass);
