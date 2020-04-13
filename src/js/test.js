dfe735d17f75cfa06c693fc3010a191affac0f80
lamyita <50775487+lamyita@users.noreply.github.com>
2020-04-13 20:34:18 +0100

    page accueil virsion mobile

---
+++ src/js/test.js
@@ -0,0 +1,12 @@
+// function myFunction(){
+// document.getElementsByClassName('tst').innerHTML="hhhhhhhhhhhhhhh";
+
+// }
+// myFunction;
+// document.getElementById('textChanger').onclick= function(){
+
+//     document.getElementById('quiz1').innerHTML="tst"
+// }
+let {
+    ""
+}
